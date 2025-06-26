deb832e266539213fc9775bc10bfc8aca90c08a2
Michael Ochoa <michaelochoa@Michaels-MacBook-Pro.local>
2025-06-26 10:49:28 -0400

    feat: Create and display Header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Header from '@/components/Header';
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Welcome to the FedReconcile Prototype</h1>
+    <main className="flex min-h-screen flex-col items-center">
+      <Header />
+      <div className="p-24">
+        <h1 className="text-4xl font-bold">Welcome to the FedReconcile Prototype</h1>
+      </div>
     </main>
   );
 }
